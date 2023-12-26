3690888c2c2d57a8cda905eb2ed4b1f94e5a3a76
donald <donald.nikkessen@gmail.com>
2023-12-26 01:48:49 +0100

    Working version with left mouse button reset

---
+++ src/game.rs
@@ -0,0 +1,117 @@
+use std::collections::HashMap;
+use rand::Rng;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
+}
+
+impl Cell {
+    pub(crate) fn kill(&mut self) {
+        *self = Cell::Dead
+    }
+    fn live(&mut self) {
+        *self = Cell::Alive
+    }
+}
+
+pub struct Game {
+    pub(crate) grid_size: (i32, i32),
+    pub(crate) cell_size: f64,
+    pub(crate) grid: Vec<Vec<Cell>>,
+    neighbors_map: HashMap<(usize, usize), u8>
+}
+
+impl Game {
+    pub fn initialize(grid_size: (i32, i32), cell_size: f64) -> Self {
+        let mut neighbors_map: HashMap<(usize, usize), u8> = HashMap::new();
+        let grid = Self::create_random_grid(grid_size);
+        Game { grid_size, cell_size, grid, neighbors_map }
+    }
+
+    pub fn create_random_grid(grid_size: (i32, i32)) -> Vec<Vec<Cell>> {
+        let mut grid = Vec::new();
+        for _ in 0..grid_size.1 {
+            let mut row = Vec::new();
+            for _ in 0..grid_size.0 {
+                row.push(match rand::thread_rng().gen::<bool>() {
+                    true => Cell::Alive,
+                    false => Cell::Dead
+                });
+            }
+            grid.push(row);
+        }
+        grid
+    }
+
+    pub fn count_neighbors(&mut self) {
+        for col in 0..self.grid_size.0 as usize {
+            for row in 0..self.grid_size.1 as usize {
+                let neighbors_to_check: Vec<(u8, u8)> = self.get_neighbors(row, col);
+                let mut positive_neighbors: u8 = 0;
+                for pos in neighbors_to_check {
+                    if self.grid[pos.0 as usize][pos.1 as usize] == Cell::Alive {
+                        positive_neighbors += 1;
+                    }
+                }
+                self.neighbors_map.insert((row, col), positive_neighbors);
+            }
+        }
+    }
+
+    fn get_neighbors(&mut self, input_col: usize, input_row: usize) -> Vec<(u8, u8)> {
+        let mut neighbors_to_check: Vec<(u8, u8)> = Vec::new();
+        let mut rows: Vec<i32> = vec![-1, 0, 1];
+        let mut cols: Vec<i32> = vec![-1, 0, 1];
+
+        if input_col == 0 {
+            cols.remove(0);
+            cols.push(self.grid_size.0 - 1);
+        }
+        else if input_col == self.grid_size.0 as usize - 1 {
+            cols.remove(cols.len() - 1);
+            cols.push(-(self.grid_size.0 - 1));
+        }
+        if input_row == 0 {
+            rows.remove(0);
+            rows.push(self.grid_size.1 - 1);
+        }
+        else if input_row == self.grid_size.1 as usize - 1 {
+            rows.remove(rows.len() - 1);
+            rows.push(-(self.grid_size.1 - 1));
+        }
+        for row in rows {
+            for col in &cols {
+                if (row, col) != (0, &0) {
+                    neighbors_to_check.push(((input_col as i32 + col) as u8, (input_row as i32 + row) as u8))
+                }
+            }
+        }
+        neighbors_to_check
+    }
+
+    pub fn apply_rules(&mut self) {
+        for col in 0..self.grid_size.0 as usize {
+            for row in 0..self.grid_size.1 as usize {
+                let mut cell = self.grid[col][row];
+                let alive_neighbors = self.neighbors_map[&(col, row)];
+                match cell {
+                    Cell::Alive => {
+                        if alive_neighbors < 2 {
+                            self.grid[col][row] = Cell::Dead;
+                        }
+                        else if alive_neighbors > 3 {
+                            self.grid[col][row] = Cell::Dead;
+                        }
+                    }
+                    Cell::Dead => {
+                        if alive_neighbors == 3 {
+                            self.grid[col][row] = Cell::Alive
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,98 @@
+#![allow(unused_comparisons)]
+
+mod game;
+use game::Cell;
+use game::Game;
+type GameType = Game;
+
 extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 extern crate rand;
 
-use std::collections::HashMap;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::{Button, ButtonEvent, ButtonState, MouseButton, MouseCursorEvent};
 use piston::window::WindowSettings;
-use rand::Rng;
-
-pub struct Grid {
-    grid_size: (u32, u32),
-    cell_size: f64,
-    grid: Vec<Vec<bool>>,
-    // neighbors_map: HashMap<(usize, usize), u32>
-}
-
-impl Grid {
-    pub fn new(grid_size: (u32, u32), cell_size: f64) -> Self {
-        let mut grid = Vec::new();
-        for _ in 0..grid_size.1 {
-            let mut row = Vec::new();
-            for _ in 0..grid_size.0 {
-                row.push(rand::thread_rng().gen::<bool>());
-            }
-            grid.push(row);
-        }
-        Grid { grid_size, cell_size, grid }
-    }
-
-    // pub fn count_neighbors(&self) {
-    //     let mut neighbors_map: HashMap<(usize, usize), u32> = HashMap::new();
-    //     let grid_size = self.grid_size;
-    //     let grid = &self.grid;
-    //
-    //     for i in 0..grid_size.0 {
-    //         for j in 0..grid_size.1 {
-    //             let mut positive_neighbors = 0;
-    //
-    //             for x in (i - 1)..(i + 2) {
-    //                 for y in (j - 1)..(j + 2) {
-    //                     if x < grid_size.0 && x >= 0 && y < grid_size.1 && y >= 0 {
-    //                         if grid[x as usize][y as usize] {
-    //                             positive_neighbors += 1;
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //             let (x, y) = (i, j);
-    //             neighbors_map.insert((x as usize, y as usize), positive_neighbors);
-    //         }
-    //     }
-    //     neighbors_map
-    // }
-}
+const MAX_FPS: i32 = 144;
+const GRID_SIZE: (i32, i32) = (100, 100);
+const CELL_SIZE: f64 = 10.0;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
-    grid: Grid
+    game: GameType
 }
 
 impl App {
 
-    fn render(&mut self, args: &RenderArgs) {
+    fn render(&mut self, _args: &RenderArgs) {
         use graphics::*;
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, self.grid.cell_size);
-        // let (x, y) = (0.0, 0.0);
+        let square = rectangle::square(0.0, 0.0, self.game.cell_size);
         // let (window_width, window_height) = (args.window_size[0], args.window_size[1]);
-        let grid_size = self.grid.grid_size;
-        let cell_size = self.grid.cell_size;
-        let grid = &self.grid.grid;
+        let cell_size = self.game.cell_size;
+        let grid = &self.game.grid;
 
-        self.gl.draw(args.viewport(), |c, gl| {
+        self.gl.draw(_args.viewport(), |c, gl| {
             // Clear the screen.
-            clear(GREEN, gl);
+            clear(BLACK, gl);
             let mut transform;
             let mut x_move;
             let mut y_move = 0.0;
-            for i in 0..grid_size.1 {
+            for row in grid {
                 let mut curr_y_move = y_move;
                 x_move = 0.0;
-                for j in 0..grid_size.0 {
+                for cell in row {
                     transform = c
                         .transform
                         .trans(x_move, curr_y_move);
-                    match grid[i as usize][j as usize] {
-                        true => rectangle(RED, square, transform, gl),
-                        false => rectangle(GREEN, square, transform, gl)
+                    if Cell::Alive.eq(cell) {
+                        rectangle(WHITE, square, transform, gl)
                     }
                     x_move += cell_size;
                 }
                 curr_y_move += cell_size;
                 y_move = curr_y_move;
-
             }
         });
+        // let mut input = String::new();
+        //
+        // io::stdin().read_line(&mut input)
+        //     .ok()
+        //     .expect("Couldn't read line");
     }
 
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-        // self.grid.count_neighbors();
+    fn update(&mut self, _args: &UpdateArgs) {
+        // self.rotation += 2.0 * args.dt;
+        self.game.count_neighbors();
+        self.game.apply_rules();
     }
 }
-type GridType = Grid;
+
 fn main() {
-    let grid: GridType = Grid::new((10, 10), 30.0);
-    run_the_game(grid);
+    println!("Starting!");
+    let game: GameType = Game::initialize(GRID_SIZE, CELL_SIZE);
+    run_the_game(game);
 }
 
-fn run_the_game(grid: Grid) {
+
+fn run_the_game(mut game: GameType) {
 // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
+    let target_fps = MAX_FPS;
+    let target_frame_duration = std::time::Duration::from_secs(1) / target_fps as u32;
 
     // Create a Glutin window.
     let mut window: Window = WindowSettings::new(
-        "spinning-square",
-        [grid.grid_size.0 as f64 * grid.cell_size, grid.grid_size.1 as f64 * grid.cell_size]
+        "Game of Life",
+        [game.grid_size.0 as f64 * game.cell_size, game.grid_size.1 as f64 * game.cell_size]
     )
         .graphics_api(opengl)
         .exit_on_esc(true)
@@ -134,12 +102,20 @@ fn run_the_game(grid: Grid) {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-        grid
+        // rotation: 0.0,
+        game
     };
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.button_args() {
+            if args.button == Button::Mouse(MouseButton::Left) {
+                if args.state == ButtonState::Press {
+                    app.game = Game::initialize(GRID_SIZE, CELL_SIZE)
+                }
+            }
+        }
+
         if let Some(args) = e.render_args() {
             app.render(&args);
         }
@@ -147,5 +123,17 @@ fn run_the_game(grid: Grid) {
         if let Some(args) = e.update_args() {
             app.update(&args);
         }
+        std::thread::sleep(target_frame_duration);
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cell_is_dead_after_kill() {
+        let mut cell: Cell = Cell::Alive;
+        cell.kill();
+        assert_eq!(Cell::Dead, cell);
     }
 }
